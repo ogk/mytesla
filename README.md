3257eb12dbf27686c3eafd739626c8cf760a5269
Olav G. Karlsen <olav@ogk.no>
2016-01-31 18:40:56 +0100

    Now actually working...

---
+++ README.md
@@ -0,0 +1,12 @@
+# mytesla
+
+Personal code for experimenting with the Tesla API, probably useless for others
+
+Thinking of registering info like odometer and other stuff on a regular basis, perhaps daily,
+to keep a history.
+
+# Credits
+
+Uses "teslajson" by Greg Glockner at https://github.com/gglockner/teslajson 
+
+Tesla API documented by Tim Dorr: http://docs.timdorr.apiary.io/#
---
+++ mytesla.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+""" Very basic beginner project to experiment with connecting top my Tesla
+using  https://github.com/gglockner/teslajson
+
+"""
+
+
+import os
+import teslajson
+import datetime
+
+def establish_connection(token=None):
+    c = teslajson.Connection(email=TESLA_EMAIL, password=TESLA_PASSWORD, access_token=token)
+    return c
+    
+def get_odometer(c, car):
+    odometer = None
+    for v in c.vehicles:
+        if v["display_name"] == car:
+            d = v.data_request("vehicle_state")
+            odometer = int(d["odometer"])
+    return odometer
+    
+def exit_if_logininfo_not_set():
+    print "One or both environment variables TESLA_EMAIL or  TESLA_PASSWORD are not set."
+    print "The must be set in the shell you are running from, e.g."
+    print "    export TESLA_EMAIL=myemail"
+    print "    export TESLA_PASSWORD=secret"
+    print "   ", os.path.basename(__file__)
+    print "or:"
+    print "    TESLA_EMAIL=myemail TESLA_PASSWORD=secret", os.path.basename(__file__)
+    print
+    quit()
+
+# Must be exported enviromnent variables set before running script
+try:
+    TESLA_EMAIL = os.environ['TESLA_EMAIL']
+    TESLA_PASSWORD = os.environ['TESLA_PASSWORD']
+except:
+    exit_if_logininfo_not_set()
+
+MY_CAR="The Batmobile"
+
+c = establish_connection()
+print "{} {} km".format(datetime.datetime.now().isoformat(), int(get_odometer(c, MY_CAR) * 1.609))
